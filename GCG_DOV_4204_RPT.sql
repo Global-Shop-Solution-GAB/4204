6979dbf485b0f4814e58e1f95c241efaaec2a50a
Service Web <sw@gssmail.com>
2024-03-11 13:40:04 -0500

    Commit changes in C:\Websites\Git\emarlow\4204\GCG_DOV_4204_RPT.sql

---
+++ GCG_DOV_4204_RPT.sql
@@ -0,0 +1,12 @@
+CREATE TABLE "GCG_DOV_4204_RPT"(
+ "Part" CHAR(25),
+ "Amt_Price" DECIMAL(13,5),
+ "Code_Sort" CHAR(10),
+ "Description" CHAR(30),
+ "MFG_Part" CHAR(23),
+ "Qty_OnHand" NUMERIC(12,4),
+ "VIM_Extend_Desc" LONGVARCHAR,
+ "Name_Vendor" CHAR(30),
+ "BIRunID" BIGINT,
+ "Qty_OnDemand" NUMERIC(12,4),
+ "DATE_LAST_USAGE" CHAR(8));
